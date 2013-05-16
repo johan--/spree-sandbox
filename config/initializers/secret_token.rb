8d8b5ec71276900e162ba878da2cf908aaf2f174
johan pretorius <johan.pretorius@gmail.com>
2013-05-16 09:12:40 +0200

    init spree store sandbox application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeStore::Application.config.secret_token = 'e6c0fd9f19954760a16f0779b396ddcf5bc10a058c918bde166616de3ace69c87d2addfedc919e93a88f0ec4774d4919544f88fd145320222377e0723ac2590d'
